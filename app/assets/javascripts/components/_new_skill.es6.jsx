b7b079229235603c5f74dae97143b892beff5dc2
Brian Sayler <sayler.b@gmail.com>
2016-09-06 00:10:51 -0600

    finally got the fetch syntax to work

--- app/assets/javascripts/components/_new_skill.es6.jsx
+++ app/assets/javascripts/components/_new_skill.es6.jsx
@@ -5,10 +5,19 @@ class NewSkill extends React.Component {
   }
 
   handleClick() {
-    let name = this.name
-    let details = this.details
+    var name = this.name.value
+    var details = this.details.value
 
-    console.log(name, details)
+    var request = new Request('/api/v1/skills.json')
+
+    fetch(request, {
+      method: 'POST',  
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify( { skill: { name: name, details: details } } ) 
+    })
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+
   }
 
   render() {
