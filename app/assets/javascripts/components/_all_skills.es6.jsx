3884e38842fa65135dbc1b62f2b1945708c72095
Brian Sayler <sayler.b@gmail.com>
2016-09-05 21:08:15 -0600

    add initial state to all skills component

--- app/assets/javascripts/components/_all_skills.es6.jsx
+++ app/assets/javascripts/components/_all_skills.es6.jsx
@@ -1,9 +1,14 @@
 class AllSkills extends React.Component {
+  getInitialState() {
+    return { skills: [] }
+  }
+
   componentDidMount() {
-    $.getJSON('/api/v1/skills.json', (response) => { console.table(response) })
+    $.getJSON('/api/v1/skills.json', (response) => { this.setState( {skills: response }) })
   }
 
   render() {
+    console.log("Current State:", this.state)
     return (
       <div>
         <h1>Hello! This is AllSkills</h1>
