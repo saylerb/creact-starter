18fa219e31390f4b5b3f85121be4af084db1d28c
Brian Sayler <sayler.b@gmail.com>
2016-09-05 21:49:45 -0600

    replaced refs with callbacks and add constructor which binds this

--- app/assets/javascripts/components/_new_skill.es6.jsx
+++ app/assets/javascripts/components/_new_skill.es6.jsx
@@ -1,8 +1,22 @@
 class NewSkill extends React.Component {
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    let name = this.name
+    let details = this.details
+
+    console.log(name, details)
+  }
+
   render() {
     return (
       <div>
-        <h1>This is a new skill</h1>
+        <input ref={ref => this.name = ref} placeholder='Enter name of skill' />
+        <input ref={ref => this.details = ref} placeholder='Details' />
+        <button onClick={this.handleClick}>Submit</button>
       </div>
     )
   }
