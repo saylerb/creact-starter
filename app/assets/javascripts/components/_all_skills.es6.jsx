aad3d3b2e5c647cc3d79f365d705d9401df5dab3
Brian Sayler <sayler.b@gmail.com>
2016-09-06 22:29:36 -0600

    move fetching skills to the Body component

--- app/assets/javascripts/components/_all_skills.es6.jsx
+++ app/assets/javascripts/components/_all_skills.es6.jsx
@@ -1,20 +1,6 @@
 class AllSkills extends React.Component {
-  constructor() {
-    super()
-    this.state = { skills: [] }
-  }
-
-  componentDidMount() {
-    let request = new Request('/api/v1/skills.json', { method: 'GET' })
-
-    fetch(request)
-      .then(response => response.json())
-      .then(response => this.setState( {skills: response }) )
-      .catch(err => console.log(err))
-  }
-
   render() {
-    let skills = this.state.skills.map(skill => {
+    let skills = this.props.skills.map(skill => {
       return (
         <div key={skill.id}>
           <h3>{skill.name}</h3>
--- app/assets/javascripts/components/_body.es6.jsx
+++ app/assets/javascripts/components/_body.es6.jsx
@@ -1,12 +1,26 @@
 class Body extends React.Component {
+  constructor() {
+    super()
+    this.state = { skills: [] }
+  }
+
+  componentDidMount() {
+    let request = new Request('/api/v1/skills.json', { method: 'GET' })
+
+    fetch(request)
+      .then(response => response.json())
+      .then(response => this.setState( {skills: response }) )
+      .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <div>
         <div>
-          < NewSkill/>
+          <NewSkill />
         </div>
         <div>
-          < AllSkills/>
+          <AllSkills skills={this.state.skills} />
         </div>
       </div>
     )
